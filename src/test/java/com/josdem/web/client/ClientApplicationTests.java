82ff647ff469db1c65cdfa874a873a8af8eb3ce5
josdem <joseluis.delacruz@gmail.com>
2023-10-01 13:30:22 -0400

    Starting new Spring web resource client project :)

---
+++ src/test/java/com/josdem/web/client/ClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.josdem.web.client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
