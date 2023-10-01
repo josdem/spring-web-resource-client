8701ca4c3a8fb5d47947de9b1cccd02598132aca
josdem <joseluis.delacruz@gmail.com>
2023-10-01 13:34:00 -0400

    Adding application context test

--- src/test/java/com/josdem/web/client/ClientApplicationTests.java
+++ src/test/java/com/josdem/web/client/ClientApplicationTests.java
@@ -1,13 +1,26 @@
 package com.josdem.web.client;
 
+import jdk.jfr.Name;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInfo;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
 
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@Slf4j
 @SpringBootTest
+@RequiredArgsConstructor
 class ClientApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+  private final ApplicationContext applicationContext;
 
+  @Test
+  @Name("Should load context")
+  void contextLoads(TestInfo testInfo) {
+    log.info("Running: {}", testInfo.getDisplayName());
+    assertNotNull(applicationContext, "should start the application");
+  }
 }
